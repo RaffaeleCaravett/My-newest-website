53117c4ed2609ff4d72b3b1cabba3227c3f8447c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-23 10:48:57 +0200

    npm installed bootstrap, aos, taoastify

--- my-personal-website/src/App.tsx
+++ my-personal-website/src/App.tsx
@@ -1,14 +1,28 @@
 
 
 import './App.css'
-
+import {BrowserRouter, Route,  Routes } from 'react-router-dom'
+import Nav from './Components/Nav'
+import Footer from './Components/Footer'
+import Home from './Components/Home'
 function App() {
  
 
   return (
-      <div>
-       
+    <BrowserRouter>
+    <Nav />
+    <div className="App">
+    <div className="Content">
+ <Routes>
+        <Route  path="/home" element={<Home />}>
+        </Route>
+        <Route path="*" element={<Home />}>
+        </Route>
+      </Routes>
       </div>
+      </div>
+      <Footer />
+</BrowserRouter>
   )
 }
 
