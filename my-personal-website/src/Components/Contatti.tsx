3913455a26b9a4688a11133781404e74e52ee22a
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-24 11:50:00 +0200

    tryind to send email

--- my-personal-website/src/Components/Contatti.tsx
+++ my-personal-website/src/Components/Contatti.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from "react";
+import emailjs from 'emailjs-com';
+import { toast } from "react-toastify";
 
 function Contatti ()  {
 
@@ -9,6 +12,58 @@ const [aziendaError,setAziendaError]= useState('')
 const [emailError,setEmailError]= useState('')
 const [commentoError,setCommentoError]= useState('')
 
+const contact = (e:any)=>{
+    e.preventDefault()
+    const nome = (document.getElementById('nome') as HTMLInputElement).value
+    const cognome = (document.getElementById('cognome') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const azienda = (document.getElementById('azienda') as HTMLInputElement).value
+    const testo= (document.getElementById('commento') as HTMLInputElement).value
+    const emailRegex = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')
+    if(nome==''||nome==undefined||nome==null){
+        setNomeError("Inserisci un valore.")
+    }else{
+        setNomeError("")
+    }
+    if(cognome==''||cognome==undefined||cognome==null){
+        setCognomeError("Inserisci un valore.")
+    }else{
+        setCognomeError("")
+    }
+    if(email==''||email==undefined||email==null){
+        setEmailError("Inserisci un valore.")
+    }else if(!emailRegex.test(email)){
+        setEmailError("Inserisci un valore tipo 'mario@rossi.com'.")
+    }else{
+        setEmailError("")
+    }
+    if(testo==''||testo==undefined||testo==null){
+        setCommentoError("Inserisci un valore.")
+    }else{
+        setCommentoError("")
+    }
+    setAziendaError('')
+
+  if(nomeError==''&&cognomeError==''&&emailError==''&&aziendaError==''&&commentoError==''&&nome&&cognome&&email&&testo){
+console.log(azienda)
+emailjs.sendForm('service_l053f3n', 'template_v9ffrwa', JSON.stringify({
+    to_name: "Raffaele",
+    from_name: nome,
+    message: testo,
+    reply_to: "Raffaele",
+    from_surname: cognome,
+    from_email: email,
+    }), 'cYGPn-yEQLuIO4Ad9')
+      .then((result) => {
+        console.log(result)
+        toast.success("Email mandata correttamente")
+          window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+      }, (error) => {
+        toast.error(error.text.toString())
+          console.log(error.text);
+      });
+  }
+}
 
 return (
     <div className="container py-5">
@@ -18,36 +73,36 @@ return (
         <p>Completa il form qui sotto per mandarmi una mail. Risponderò il prima possibile.</p>
     </div>
     <div className="col-md-12">
-        <form action="" className="w-75 m-auto">
+        <form action="" className="w-75 m-auto" onSubmit={(e)=>contact(e)}>
             <div className="row">
                 <div className="col-md-6">
                     <label htmlFor="" className="py-3">Nome</label>
             <input type="text" className="form-control" placeholder="Inserisci il nome" id="nome"/>
-            {nomeError!=''&&<p className="text-danger"></p>}
+            {nomeError!=''&&<p className="text-danger">{nomeError}</p>}
                 </div>
                 <div className="col-md-6">
             <label htmlFor="" className="py-3">Cognome</label>
             <input type="text" className="form-control" placeholder="Inserisci il cognome" id="cognome"/>
-            {cognomeError!=''&&<p className="text-danger"></p>}
+            {cognomeError!=''&&<p className="text-danger">{cognomeError}</p>}
                 </div>
                  <div className="col-md-6">
                  <label htmlFor="" className="py-3">Nome azienda (opzionale)</label>
             <input type="text" className="form-control" placeholder="Inserisci il nome dell'azienda" id="azienda"/>
-            {aziendaError!=''&&<p className="text-danger"></p>}
+            {aziendaError!=''&&<p className="text-danger">{aziendaError}</p>}
             </div>
             <div className="col-md-6">
             <label htmlFor="" className="py-3">Email</label>
             <input type="email" className="form-control" placeholder="Inserisci l'email" id="email"/>
-            {emailError!=''&&<p className="text-danger"></p>}
+            {emailError!=''&&<p className="text-danger">{emailError}</p>}
             </div>
             <div className="col-md-12">
                 <label htmlFor="" className="py-3">Testo</label>
                 <textarea className="form-control" placeholder="Lascia un commento" id="commento">
                 </textarea>
-                {commentoError!=''&&<p className="text-danger"></p>}
+                {commentoError!=''&&<p className="text-danger">{commentoError}</p>}
             </div>
             </div>
-<button className="btn py-5" type="submit">Invia</button>
+<button className="shadow-none btn py-5" type="submit">Invia</button>
         </form>
     </div>
 </div>
